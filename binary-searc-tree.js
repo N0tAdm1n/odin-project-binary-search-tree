affa15662d9dd9b162ece79e24e9b544e6d42489
N0tAdm1n <n0tadm1n29@gmail.com>
2023-01-01 17:02:30 +0530

    Add Tree() factory

--- binary-searc-tree.js
+++ binary-searc-tree.js
@@ -1,7 +1,25 @@
+import { mergeSort } from "./merge-sort";
+
 function Node(value = null, leftNode = null, rightNode = null) {
   return {
     value,
     leftNode,
     rightNode,
+  };
+}
+
+function Tree(array) {
+  let root = buildTree();
+  let sortedArray = mergeSort(array);
+
+  //   function buildTree(array) {
+  //       let mid =
+
+  //   }
+
+  return {
+    get root() {
+      return root;
+    },
   };
 }
